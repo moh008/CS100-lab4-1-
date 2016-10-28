f8d4e6b6245373445058915d68e90ca1002071f7
moh008 <moh008@ucr.edu>
2016-10-28 15:19:24 -0700

    change syntax format

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -8,23 +8,23 @@ using namespace std;
 BubbleSort::BubbleSort(){}
 
 void BubbleSort::sort(Container* container){
-int i, j;// flag = 1;
-int numLength = container->size();
+	int i, j;// flag = 1;
+	int numLength = container->size();
 
-for(i = 0; i <= numLength; i++)
-{
-	//cout << "bubble" << endl;
-	//flag = 0;
-	for(j = 0; j < (numLength - 1); j++)
+	for(i = 0; i <= numLength; i++)
 	{
-		if(container->at(j+1)->evaluate() <  container->at(j)->evaluate())
+		//cout << "bubble" << endl;
+		//flag = 0;
+		for(j = 0; j < (numLength - 1); j++)
 		{
-			//cout << "Swap" << endl;
-			container->swap(j,(j+1));
-			//flag = 1;
+			if(container->at(j+1)->evaluate() <  container->at(j)->evaluate())
+			{
+				//cout << "Swap" << endl;
+				container->swap(j,(j+1));
+				//flag = 1;
+			}
 		}
+		return;
 	}
-	return;
-}
-}
+};
 
